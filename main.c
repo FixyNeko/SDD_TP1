69f4d3ceb88ec781bff1dc91705ead91e00c1eab
FixyNeko <felix@dhumes.fr>
2019-03-13 19:20:37 +0100

    modification fonction

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 int main(int argc, char ** argv) {
 	liste_t liste;
 
-	char* NomFichierLecture; // Nom du fichier lu, passé en 1er paramètre du programme
-	char* NomFichierEcriture; // Nom du fichier écrit, passé en 2eme paramètre du programme
+	char* NomFichierLecture; /* Nom du fichier lu, passé en 1er paramètre du programme */
+	char* NomFichierEcriture; /* Nom du fichier écrit, passé en 2eme paramètre du programme */
 
 	if(argc == 2) {
 		NomFichierLecture = argv[1];
@@ -20,24 +20,10 @@ int main(int argc, char ** argv) {
 
 	/* Traitement */
 
-	printf("\nchaine vide\n\n");
-	InitLCH(&liste);
-	LectureFichier("Tests/FichierVide.txt", &liste);
-	MessagesContenantMotif(&liste, "message depassé");
-
-	SupprimerLCH(&liste);
-
-	printf("\nmotif vide\n\n");
 	InitLCH(&liste);
 	LectureFichier("Tests/FichierNormal.txt", &liste);
-	MessagesContenantMotif(&liste, "");
-
-	SupprimerLCH(&liste);
-
-	printf("\nmotif et chaine non vides\n\n");
-	InitLCH(&liste);
-	LectureFichier("Tests/FichierNormal.txt", &liste);
-	MessagesContenantMotif(&liste, "message depassé");
+	ModifierDateDebut(&liste, 19851030, 11111111);
+	AfficherMessagesValides(&liste);
 
 	SupprimerLCH(&liste);
 
